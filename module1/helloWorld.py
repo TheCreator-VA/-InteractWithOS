966149aef5db4d52b14a8f8ad716510adf166866
creator <vansharora2929@gmail.com>
2020-05-18 04:12:43 +0530

    Executing Python code

---
+++ module1/helloWorld.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Adding the above line and changing the mode of file to executable.
+# Code can be executed by typing ./fileName.py
+# Here . represents the current directory.
+print("hello world")
