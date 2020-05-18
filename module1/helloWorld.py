c51823aa5b8883a65b6836c923728d8af4cd42b9
creator <vansharora2929@gmail.com>
2020-05-18 20:34:45 +0530

    Commented the purpose of shebang.

--- module1/helloWorld.py
+++ module1/helloWorld.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Adding the above line and changing the mode of file to executable.
+# Adding the above line tells our script to use python interpreter.
+# Add executable permission to file (chmod +x fileName)
 # Code can be executed by typing ./fileName.py
 # Here . represents the current directory.
 print("hello world")
