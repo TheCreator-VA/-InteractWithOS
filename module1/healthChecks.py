c05ad8e10749c2411efd0ba4d13db075d3ce199a
creator <vansharora2929@gmail.com>
2020-05-19 08:38:33 +0530

    Added a function to check cpu usage
    The function returns true if cpu usage is less than 75%, false otherwise.

--- module1/healthChecks.py
+++ module1/healthChecks.py
@@ -3,6 +3,7 @@
 """A script that contains several functions which check the health of the system."""
 
 import shutil
+import psutil
 
 def check_disk_usage(disk):
     # The function returns true if the disk is more than 20% free, false otherwise.
@@ -10,3 +11,7 @@ def check_disk_usage(disk):
     free = du.free / du.total * 100 # Find % free space
     return free > 20 
 
+def check_cpu_usage():
+    # The function returns true if cpu usage is less than 75%, false otherwise.
+    usage = psutil.cpu_percent(1) # Check the cpu usage for 1 second.
+    return usage < 75
