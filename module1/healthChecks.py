5803b018cf52461a0ae55b493af98b96cddcc63c
creator <vansharora2929@gmail.com>
2020-05-19 10:23:13 +0530

    Calling the functions

--- module1/healthChecks.py
+++ module1/healthChecks.py
@@ -15,3 +15,10 @@ def check_cpu_usage():
     # The function returns true if cpu usage is less than 75%, false otherwise.
     usage = psutil.cpu_percent(1) # Check the cpu usage for 1 second.
     return usage < 75
+
+# Main body to check what the functions return
+
+if not check_disk_usage("/") or not check_cpu_usage(): # if any function doesn't return true give error message
+    print("ERROR!")
+else:
+    print("Everything is OK!")
