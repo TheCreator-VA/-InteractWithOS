d57ffbb76c9597f3bfcc5e0902a04a1ec6f0a140
creator <vansharora2929@gmail.com>
2020-05-19 08:32:39 +0530

    Added check_disk_usage function
    The function returns true if the disk is more than 20% free, false otherwise.

--- module1/healthChecks.py
+++ module1/healthChecks.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-""" A script that contains several functions which check the health of the system."""
+"""A script that contains several functions which check the health of the system."""
+
+import shutil
+
+def check_disk_usage(disk):
+    # The function returns true if the disk is more than 20% free, false otherwise.
+    du = shutil.disk_usage(disk) # disk usage is a function in shutil module
+    free = du.free / du.total * 100 # Find % free space
+    return free > 20 
+
