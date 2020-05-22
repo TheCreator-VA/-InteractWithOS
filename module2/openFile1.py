689788f404f3fa8b1ed53684be5f062a5d801377
Vansh <vansharora2929@gmail.com>
2020-05-22 19:50:24 +0530

    Files moved

--- module2/openFile1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-
-# using the with option eliminated the need of explicily closing the file.
-# As soon as the block ends file is automatically closed.
-with open("output.txt") as file:
-    for line in file:
-        print(line.upper())
