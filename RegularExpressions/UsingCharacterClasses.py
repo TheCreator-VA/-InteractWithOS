bd5a94c140ad3a844962e786767239d6ad82ae14
Vansh <vansharora2929@gmail.com>
2020-05-25 12:49:10 +0530

    Search for a specific word but it can start with either an upper case or lower case character.

---
+++ RegularExpressions/UsingCharacterClasses.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import re
+
+# Using
+s1 = "Python is a cool language" # capital P
+s2 = "python is a cool language" # small p
+
+print(re.search(r"[Pp]ython",s1)) # <_sre.SRE_Match object; span=(0, 6), match='Python'>
+print(s1[0:6]) # Python
+
+print(re.search(r"[Pp]ython",s2))
+print(s2[0:6]) # python
