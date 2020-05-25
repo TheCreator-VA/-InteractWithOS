89f964ea2bca30ae460da4983c694186f95ec511
Vansh <vansharora2929@gmail.com>
2020-05-25 13:07:32 +0530

    look in a range of samll alphabets.

--- RegularExpressions/UsingCharacterClasses.py
+++ RegularExpressions/UsingCharacterClasses.py
@@ -11,3 +11,8 @@ print(s1[0:6]) # Python
 
 print(re.search(r"[Pp]ython",s2))
 print(s2[0:6]) # python
+
+# Look for a character from a range of characters
+
+s3 = "there is always an alternative way to stay away from trouble."
+print(re.search("[a-z]way",s3))   # <_sre.SRE_Match object; span=(10, 14), match='lway'>
