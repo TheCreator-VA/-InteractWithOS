2be6c503c74b6d8d4c489cc0f0483d11bfa2c4ae
Vansh <vansharora2929@gmail.com>
2020-07-10 23:15:00 +0530

    Fixed typo n comment

--- RegularExpressions/get_date_id.py
+++ RegularExpressions/get_date_id.py
@@ -1,4 +1,4 @@
-# Det date and id
+# Get date and id
 import re
 pattern = r"(\w+ [1-3]?[0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]).*\[(\d+)\]" # Capture groups used
 line = "Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)"
