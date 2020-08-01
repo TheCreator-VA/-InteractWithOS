d56a8f4d6865b67ba78b9224f540b830228e36a7
Vansh <vansharora2929@gmail.com>
2020-08-01 16:47:21 +0530

    Commented print statements

--- RegularExpressions/repeating_vowels.py
+++ RegularExpressions/repeating_vowels.py
@@ -5,17 +5,17 @@ def multi_vowel_words(text):
   result = re.findall(pattern, text)
   return result
 
-print(multi_vowel_words("Life is beautiful")) 
+#print(multi_vowel_words("Life is beautiful")) 
 # ['beautiful']
 
-print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+#print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
 # ['Obviously', 'queen', 'courageous', 'gracious']
 
-print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+#print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
 # ['rambunctious', 'quietly', 'delicious']
 
-print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+#print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
 # ['queue']
 
-print(multi_vowel_words("Hello world!")) 
+#print(multi_vowel_words("Hello world!")) 
 # []
