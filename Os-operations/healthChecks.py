1b887095c47472cc2b6539766d30abc315599313
Vansh <vansharora2929@gmail.com>
2020-08-02 20:00:00 +0530

    Defined task

---
+++ Os-operations/healthChecks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""A script that contains several functions which check the health of the system."""
+
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    # The function returns true if the disk is more than 20% free, false otherwise.
+    du = shutil.disk_usage(disk) # disk usage is a function in shutil module
+    free = du.free / du.total * 100 # Find % free space
+    return free > 20 
+
+def check_cpu_usage():
+    # The function returns true if cpu usage is less than 75%, false otherwise.
+    usage = psutil.cpu_percent(1) # Check the cpu usage for 1 second.
+    return usage < 75
+
+# Main body to check what the functions return
+
+if not check_disk_usage("/") or not check_cpu_usage(): # if any function doesn't return true give error message
+    print("ERROR!")
+else:
+    print("Everything is OK!")
---
+++ Os-operations/helloWorld.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Adding the above line tells our script to use python interpreter.
+# Add executable permission to file (chmod +x fileName)
+# Code can be executed by typing ./fileName.py
+# Here . represents the current directory.
+print("hello world")
