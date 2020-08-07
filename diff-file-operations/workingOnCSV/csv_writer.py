c418599ccba332ae0966d7d417f5d98448ba933e
Vansh <vansharora2929@gmail.com>
2020-08-07 13:00:00 +0530

    Added output sample

--- diff-file-operations/workingOnCSV/csv_writer.py
+++ diff-file-operations/workingOnCSV/csv_writer.py
@@ -8,3 +8,11 @@ hosts_list = [["workstation.local", "192.168.25.31"],["webserver.cloud", "10.2.5
 with open("hosts.csv","w") as f:
     writer = csv.writer(f)
     writer.writerows(hosts_list) # Each sub-list will be written in a seperate row
+
+
+
+# CSV file created would be like this:
+
+    # workstation.local,192.168.25.31
+
+    # webserver.cloud,10.2.5.6
