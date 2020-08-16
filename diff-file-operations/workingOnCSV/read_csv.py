69c5b71cf06914b338ac638e975de4e3b72ebfde
Vansh <vansharora2929@gmail.com>
2020-08-16 13:00:00 +0530

    Task specified

---
+++ diff-file-operations/workingOnCSV/read_csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Read a csv file
+# Print its content in some format
+
+import csv
+
+f = open("csv_file.txt")
+csv_f = csv.reader(f)
+for row in csv_f:
+    name, phone, roll = row
+    print(f"Name: {name}, Phone: {phone}, RollNo: {roll}")
+     
+f.close()
