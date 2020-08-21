6b8381ec3c082f17a2298a059235aaf7b5a5e717
Vansh <vansharora2929@gmail.com>
2020-08-21 13:20:00 +0530

    Scrit complete

--- diff-file-operations/workingOnCSV/read_with_dictionary.py
+++ diff-file-operations/workingOnCSV/read_with_dictionary.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # Read the contents of a csv file as a dictionary
+
+import csv
+
+with open('software.csv') as software:
+    reader = csv.DictReader(software)
+    for row in reader:
+        print("{} has {} users".format(row["name"], row["users"]))
