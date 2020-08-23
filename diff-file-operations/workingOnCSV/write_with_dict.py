80496a554d1ceab1599ea054134d9f08744e4c12
Vansh <vansharora2929@gmail.com>
2020-08-23 12:20:00 +0530

    Dictionary to be written

--- diff-file-operations/workingOnCSV/write_with_dict.py
+++ diff-file-operations/workingOnCSV/write_with_dict.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # Write the contents of a dictionary to a csv file.
+import csv
+
+sudents =[{"name" : "Vansh", "username" : "creator", "roll" : 120},
+    {"name" : "Karan", "username" : "sun", "roll" : 121},
+    {"name" : "Arjun", "username" : "rj", "roll" : 111}]
+
