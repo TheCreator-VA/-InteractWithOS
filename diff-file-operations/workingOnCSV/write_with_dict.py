2265a2a3d9e2025c53501f94a4c66ad7338c37ee
Vansh <vansharora2929@gmail.com>
2020-08-24 12:20:00 +0530

    Scrit complete

--- diff-file-operations/workingOnCSV/write_with_dict.py
+++ diff-file-operations/workingOnCSV/write_with_dict.py
@@ -6,3 +6,8 @@ sudents =[{"name" : "Vansh", "username" : "creator", "roll" : 120},
     {"name" : "Karan", "username" : "sun", "roll" : 121},
     {"name" : "Arjun", "username" : "rj", "roll" : 111}]
 
+keys = ["name", "username", "roll"]
+with open("student_username.csv","w") as f:
+    writer = csv.DictWriter(f, fieldnames = keys)
+    writer.writeheader()
+    writer.writerows(sudents)
