32beb827dc3579c1075a978787918b19bdb03713
Vansh <vansharora2929@gmail.com>
2020-08-27 12:20:00 +0530

    Basic directory operations cont.

--- diff-file-operations/workingOnDirectories/dir_operations.py
+++ diff-file-operations/workingOnDirectories/dir_operations.py
@@ -4,3 +4,18 @@ import os
 # get current wprking dirctory
 print(os.getcwd())
 
+# make directory
+os.mkdir("our_dir")
+
+# switch directory
+os.chdir("our_dir")
+print(os.getcwd())
+
+os.mkdir("new_Dir")
+
+# List the contents of current working directory
+print(os.listdir())
+
+# Delete directory
+os.rmdir("new_dir")
+print(os.listdir())
