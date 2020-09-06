5b35fd49f7461766e06b3ca0ad9e01a03053b9c6
Vansh <vansharora2929@gmail.com>
2020-09-06 10:00:00 +0530

    Checking for files and directories

--- diff-file-operations/workingOnDirectories/iterate_directory_content.py
+++ diff-file-operations/workingOnDirectories/iterate_directory_content.py
@@ -7,3 +7,14 @@ for n in os.listdir():
     n = os.path.join(os.getcwd(),n)
     # getcwd() returns the absolute path.   
     # Join takes the name of contents and joins it to the absolute path of parent.
+    
+    # Check for directory.
+    if os.path.isdir(n):
+        print("{} is directory.".format(n))
+
+    # Check for file.
+    elif os.path.isfile(n):
+        print(f"{n} is a file.")
+
+    else:
+        print(f"{n} is neither a file nor a directory.")
