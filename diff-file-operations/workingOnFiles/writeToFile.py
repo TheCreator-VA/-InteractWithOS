b15b727d701cbb687ff17874ea8175f6bd63a64c
Vansh <vansharora2929@gmail.com>
2020-09-07 10:00:00 +0530

    Mentioned task

---
+++ diff-file-operations/workingOnFiles/writeToFile.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# python would erase all content and write new data if dile with this name exists.
+# Otherwise a new file with the given name will be created in the same folder as the file is created.
+# We can also use absolute path to create file at any other location.
