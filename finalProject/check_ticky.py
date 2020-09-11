cc9fba418b9622fd261588f79758a82a25958b68
Vansh <vansharora2929@gmail.com>
2020-09-11 16:24:54 +0530

    Write error logs to a file

--- finalProject/check_ticky.py
+++ finalProject/check_ticky.py
@@ -2,4 +2,8 @@ import re
 
 error = r"ERROR"
 file = open("syslog.log","r")
+file2 = open("errorlogs.log","w+") # create a file for error logs
 
+for line in file:
+    if re.search(error, line):
+        file2.write(line)
