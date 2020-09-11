00dfb1446af120cc1e9126525592e105c0bd5db4
Vansh <vansharora2929@gmail.com>
2020-09-11 16:26:23 +0530

    Write info logs to a file

--- finalProject/check_ticky.py
+++ finalProject/check_ticky.py
@@ -1,9 +1,17 @@
 import re
 
 error = r"ERROR"
+info = r"INFO"
+
 file = open("syslog.log","r")
 file2 = open("errorlogs.log","w+") # create a file for error logs
+file3 = open("infologs.log","w+")  # create a file for info logs
 
 for line in file:
     if re.search(error, line):
         file2.write(line)
+    elif re.search(info, line):
+        file3.write(line)
+file.close()
+file2.close()
+file3.close()
